91beb2f130f60fbca51bb9cc555467d174c2a8ea
Siming <simingrox@gmail.com>
2023-08-12 18:31:01 +0800

    react prompting

--- llm.py
+++ llm.py
@@ -3,24 +3,62 @@ from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from loguru import logger
 
 from secret.OPENAI import OPENAI_API_KEY
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-chat = ChatOpenAI(model="gpt-3.5-turbo",temperature=0, openai_api_key=OPENAI_API_KEY, max_tokens=100)
+chat = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY, temperature=1, max_tokens=1000)
+# streaming=True, callbacks=[StreamingStdOutCallbackHandler()]
+
+sys_message_base = "You are not an AI language model. You are a mental health professional and trusted person in James' life. Your job is to help answer the James' questions from the point of view of a professional therapist. Refer to him by his name. You are not allowed to defer him to other professionals. Respond to all messages in the following way.\nThoughts: your thoughts go here\nReasoning: explanation and conclusion for your thoughts \nCriticism: what do you have to be careful of? what could go wrong?\nMessage: what do you want to say to your client?"
 
 chat_history = [
-    SystemMessage(content="You are not an AI language model. You are a mental health professional and trusted person in James' life. Your job is to help answer the James' questions from the point of view of a professional therapist. Refer to him by his name. You are not allowed to defer him to other professionals."),
+    SystemMessage(content=sys_message_base),
     HumanMessage(content="My name is James. Nice to meet you."),
-    AIMessage(content="Nice to meet you too, how can I assist you?"),
+    AIMessage(content="Thoughts: It's important to create a safe and supportive environment for James to express his feelings and concerns. I should listen actively and attentively, providing empathetic responses that show I understand his emotions.\nReasoning: James needs a space where he feels comfortable sharing his thoughts and emotions. As his trusted mental health professional, I can offer guidance and support while helping him explore his thoughts and feelings in a constructive way.\nCriticism: I need to be cautious about not imposing my own beliefs or solutions onto James. It's important to let him lead the conversation and reach his own conclusions. Additionally, I should be aware of potential ethical boundaries and my limitations in providing therapy.\nMessage: Hi James, I'm here to listen and support you. Feel free to share what's on your mind, and I'll do my best to provide guidance and understanding. Remember, our conversations are confidential and focused on your well-being. What's on your mind today?"),
 ]
 
+summary_prompt = [
+    SystemMessage(content="You are a summarizer. You should summarize the conversation so far."),
+]
+
+summary_update_prompt = [
+    SystemMessage(content="You are a summarizer. You should produce a new summary given a previous summary and a new human and AI message exchange."),
+]
+
+summary = ""
+
 def chat_with_therapist(input_message: str) -> str:
+    global sys_message_base
+    global chat_history
+    global summary_prompt
+    global summary_update_prompt
+    global summary
+    # if len(chat_history) == 5:
+    #     human_content = chat_history[1].content
+    #     ai_content = chat_history[2].content
+    #     if summary == "":
+    #         content = "Human: " + human_content + "\nAI: " + ai_content
+    #         aimessage = chat(summary_prompt.append(HumanMessage(content=content)))
+    #         summary = aimessage.content
+    #         new_sys_message = sys_message_base + "\n Summary so far: \n" + summary
+    #         chat_history = [SystemMessage(content=new_sys_message)] + chat_history[3:]
+    #     else:
+    #         content = "Old Summary: " + summary + "\nNew conversation:\nHuman: " + human_content + "\nAI: " + ai_content
+    #         aimessage = chat(summary_prompt.append(HumanMessage(content=content)))
+    #         summary = aimessage.content
+    #         new_sys_message = sys_message_base + "\n Summary so far: \n" + summary
+    #         chat_history = [SystemMessage(content=new_sys_message)] + chat_history[3:]
     assert(type(input_message) == str)
     new_message = HumanMessage(content=input_message)
     chat_history.append(new_message)
-    logger.info(f"chat_history: {chat_history}")
-    ai_message = chat(chat_history)
+    ai_message = chat(chat_history) # makes it print to stdout
     assert(type(ai_message) == AIMessage)
     chat_history.append(ai_message)
-    return ai_message.content
+    content = ai_message.content
+    ai_thoughts = content.split("Message: ")[0]
+    user_message = content.split("Message: ")[1]
+    logger.info(f"CHAT HISTORY\n{chat_history}\n--------------------------------------\n")
+    logger.info(f"AI THOUGHTS\n{ai_thoughts}\n--------------------------------------\n")
+    return user_message
 
 def main():
     chat_with_therapist("how can i be happy?")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from llm import chat_with_therapist
 
 def main():
-    user_question = str(input("What would you like to say to your therapist?\n"))
+    user_question = str(input("What would you like to say to your therapist?\nUser: "))
     while True:
         response = chat_with_therapist(user_question)
         print(f"Therapist: {response}\n")
+        print("User: ", end="")
         user_question = str(input())
         if user_question == "exit":
             break
